e219d94cd1682fed63c956c7f0b9c7ea016591dd
Fadi <fqf1fqf@gmail.com>
2022-10-27 13:51:35 +0300

    CSSS project

---
+++ CarRental.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+//     • A menu that lets a user choose whether to add a new customer or a new vehicle, print all customers’ or vehicles’ information, or exit the program, like the one shown here:
+// Choose one of the following options:
+// 1 - to add a new customer
+// 2 - to add a new vehicle
+// 3 - to print all customers’ information
+// 4 - to print all vehicles’ information
+// 5 - to quit
+
+
+
+
+
+
+// • If the user chooses option 1, the program will ask the user to enter new customer information. Customer information should be maintained in array data structures. Customer information includes the following:
+//         ▪ Customer ID number
+//         ▪ Name
+//         ▪ Phone number
+
+
+
+
+
+
+// • If the user chooses option 2, the program will ask the user to enter new vehicle information. Vehicle information should be maintained in array data structures. Vehicle information includes the following:
+//     ▪ Model year
+//     ▪ Make
+//     ▪ Model name
+//     ▪ License plate number
+//     ▪ Customer number who is currently renting vehicle
+
+
+
+
+
+
+
+// • If the user chooses option 3, the program will print all customers’ information (in tabular format), including the following fields:
+// ▪ Customer ID number
+// ▪ Name
+// ▪ Phone number
+
+
+
+
+
+
+// • If the user chooses option 4, the program will print all vehicles’ information (in tabular format), including the following fields:
+//         ▪ Model year + Make + Model name
+//         ▪ License plate number
+//         ▪ Customer number who is currently renting vehicle
+
+
+
+
+
+
+// • Please note that the program will display the same menu again after performing the required operations in options 1,2,3 and 4.
+// • If the user chooses option 5, the program exits.
